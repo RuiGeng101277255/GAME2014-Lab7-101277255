72693872a8abc9d492e24c3167d0768a6fcc53d2
RuiGeng <ruichen.gengli@georgebrown.ca>
2021-11-03 20:02:29 -0400

    added raycast for the isgrounded check

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,6 +7,10 @@ public class PlayerBehaviour : MonoBehaviour
     [Header("Movement")]
     public float HorizontalForce;
     public float VerticalForce;
+    public bool isGrounded;
+    public Transform groundOrigin;
+    public float groundOriginRadius;
+    public LayerMask groundLayerMask;
 
     private Rigidbody2D playerRB;
 
@@ -20,26 +24,44 @@ public class PlayerBehaviour : MonoBehaviour
     void Update()
     {
         Move();
+        checkIfGrounded();
     }
 
     private void Move()
     {
-        float delTime = Time.deltaTime;
-
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-        float jump = Input.GetAxisRaw("Jump");
-
-        Vector2 worldTouch = new Vector2();
-
-        foreach(var touch in Input.touches)
+        if (isGrounded)
         {
-            worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
+            float delTime = Time.deltaTime;
+
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
+            float jump = Input.GetAxisRaw("Jump");
+
+            Vector2 worldTouch = new Vector2();
+
+            foreach (var touch in Input.touches)
+            {
+                worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
+            }
+
+            float HorizontalMoveForce = x * HorizontalForce * delTime;
+            float JumpMoveForce = jump * VerticalForce * delTime;
+
+            playerRB.AddForce(new Vector2(HorizontalMoveForce, JumpMoveForce));
+            playerRB.velocity *= 0.98f;
         }
+    }
 
-        float HorizontalMoveForce = x * HorizontalForce * delTime;
-        float JumpMoveForce = jump * VerticalForce * delTime;
+    private void checkIfGrounded()
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(groundOrigin.position, groundOriginRadius, Vector2.down, groundOriginRadius, groundLayerMask);
 
-        playerRB.AddForce(new Vector2(HorizontalMoveForce, JumpMoveForce));
+        isGrounded = (hit) ? true : false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundOrigin.position, groundOriginRadius);
     }
 }
