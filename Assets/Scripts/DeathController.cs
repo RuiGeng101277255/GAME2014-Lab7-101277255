84047b04b9381cfa83435dd3cc4a995160163648
RuiGeng <ruichen.gengli@georgebrown.ca>
2021-11-11 06:58:49 -0500

    added death platform and respawn

---
+++ Assets/Scripts/DeathController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathController : MonoBehaviour
+{
+    public Transform PlayerSpawnPoint;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.position = PlayerSpawnPoint.position;
+        }
+    }
+}
